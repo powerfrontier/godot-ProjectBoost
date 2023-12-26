eaec1a98b612b45da11c671dcf10c1d40e2b4220
frontier <power_frontier@yahoo.es>
2023-12-26 03:48:42 +0100

    up to lesson 11

--- Player.gd
+++ Player.gd
@@ -1,6 +1,10 @@
-extends Node3D
+extends RigidBody3D
 
-var aux: int = 0
+## How much vertical force is applied
+@export_range(500.0, 2000.0) var thrust: float = 1000.0
+## How much left or right torque is applied
+@export_range(50.0, 200.0) var torque_thrust: float = 100.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,9 +12,25 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
-		position.y += delta
-	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
-	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)
+	if Input.is_action_pressed("boost"):
+		apply_central_force(basis.y * delta * thrust)
+	if Input.is_action_pressed("rotate_left"):
+		apply_torque(Vector3(0, 0, torque_thrust * delta))
+	if Input.is_action_pressed("rotate_right"):
+		apply_torque(Vector3(0, 0, -torque_thrust * delta))
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Goal"):
+	#if "Goal" in body.get_groups():
+		complete_level()
+	elif body.is_in_group("GameOver"):
+		crash_sequence()
+
+func crash_sequence() -> void:
+	print("GameOver")
+	get_tree().reload_current_scene()
+
+func complete_level() -> void:
+	print("level completed")
+	get_tree().quit()
