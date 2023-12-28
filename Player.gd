4d90cc34544aad4fa1335fd45f33ce03a243fcf8
frontier <power_frontier@yahoo.es>
2023-12-28 21:50:05 +0100

    up to lesson 14

--- Player.gd
+++ Player.gd
@@ -5,6 +5,8 @@ extends RigidBody3D
 ## How much left or right torque is applied
 @export_range(50.0, 200.0) var torque_thrust: float = 100.0
 
+var is_transitioning: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -21,16 +23,26 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body):
-	if body.is_in_group("Goal"):
-	#if "Goal" in body.get_groups():
-		complete_level()
-	elif body.is_in_group("GameOver"):
-		crash_sequence()
+	if !is_transitioning:
+		if body.is_in_group("Goal"):
+		#if "Goal" in body.get_groups():
+			complete_level(body.file_path)
+		elif body.is_in_group("GameOver"):
+			crash_sequence()
 
 func crash_sequence() -> void:
 	print("GameOver")
-	get_tree().reload_current_scene()
+	#For stop to call de _process function and not move the player
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().reload_current_scene)
 
-func complete_level() -> void:
+func complete_level(next_level_file: String) -> void:
 	print("level completed")
-	get_tree().quit()
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().change_scene_to_file.bind(next_level_file))
